e7f6acc9d3fb82424df99dfa0816aae72c0f1bb6
linneaAjger <linnea.ajger@gmail.com>
2022-09-20 15:35:24 +0200

    created a function that shows album title for each item

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import data from './data.json';
+import {Header} from './Components/Header'
+import {AlbumCover} from './Components/Releases'
+import {AlbumName} from './Components/Releases'
+import {ArtistName} from './Components/Releases'
 
 console.log(data);
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <>
+        <div>
+      <Header/>
     </div>
+    {data.albums.items.map((item)=> {
+      return <AlbumName name = {item.name}/>
+        // {item.artists.map((artist) => {
+        //   return <ArtistName name = {artist.name}/>
+        // })}
+      })} 
+   
+    </>
   );
 }
---
+++ code/src/Components/Header.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Header = (props) => {
+    return (
+            <h2>New albums & singels</h2>
+    )
+}

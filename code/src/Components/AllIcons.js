759ed9165f03de53c0f04bfc49b322983b30211b
Maria Westling <maria_westling@hotmail.com>
2023-01-31 18:56:03 +0100

    changes

---
+++ code/src/Components/AllIcons.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import heart from './icons/heart.svg'
+import play from './icons/play.svg'
+import dots from './icons/dots.svg'
+
+
+export const AllIcons = () => {
+    return (
+        <div className="iconsContainer">
+            <img className="heartIcon" src={heart} alt="heart icon" />
+            <img className="playIcon" src={play} alt="play icon" />
+            <img className="dotsIcon" src={dots} alt="dots icon" />
+        </div>
+    );
+};

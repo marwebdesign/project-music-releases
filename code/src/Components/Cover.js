b5bb9c7c6e31d9acf9b64b7b9ac2d10614bb0c75
Maria Westling <maria_westling@hotmail.com>
2023-03-01 11:02:53 +0100

    css changes

--- code/src/Components/Cover.js
+++ code/src/Components/Cover.js
@@ -5,9 +5,7 @@ export const Cover = (props) => {
     return (
         <>
             <img src={props.image}></img>
-            <div className="iconsContainer">
-                <AllIcons />
-            </div>
+            <AllIcons />
         </>
     )
 }
--- code/src/Components/Header.js
+++ code/src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-export const Header = (props) => {
+export const Header = () => {
     return (
-            <h2>New albums & singels</h2>
+        <h2>New albums & singels</h2>
     )
 }
